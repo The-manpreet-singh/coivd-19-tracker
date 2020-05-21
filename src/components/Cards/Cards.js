a31f4870392d1b74284aa24c1d4daabd05e090a0
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-05-21 14:32:40 +0530

    add cards

---
+++ src/components/Cards/Cards.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import classes from './Cards.module.css';
+
+const cards = (props) => {
+    return (
+        <div className={classes.container}>
+
+        </div>
+
+    );
+}
