b42f69e59f29cc86c9fd11764efe5267f6911de8
manpreet-singh10196 <manpreet.singh10196@gmail.com>
2020-05-21 14:21:00 +0530

    add imgaes

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import React from 'react';
 
 import classes from './App.module.css';
-import image from './images/image.png';
+import image from './assets/image.png';
+import Cards from './components/Cards/Cards';
 
 const app = () => {
   return (
        <div className={classes.container}> 
-            <img className={classes.image} src={image} alt='covid19' />
-             
+             <img className={classes.image} src={image} alt='covid19' />
+             {/* <Cards /> */}
          </div>
      );
-  }
-export default app; 
+};
+export default app;
